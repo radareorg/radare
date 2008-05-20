c77a78a257239fd69e7ab9e4912bc9358bbc0646
pancake <youterm.com>
2008-05-20 17:16:25 +0200

    * Oops. sparc stuff was not added O:)

---
+++ src/arch/sparc/gnudisparc.c
@@ -0,0 +1,71 @@
+/* trampoline to the gnu disassembler for sparc */
+#include <utils.h>
+#include <string.h>
+#include <radare.h>
+#include <dis-asm.h>
+#include "opcode/sparc.h"
+
+int sparc_mode = 32;
+
+static char str[128];
+extern int cons_fprintf(void *stream, const char *format, ...);
+static unsigned long Offset = 0;
+//unsigned char *bytes = "\xe1\x2f\xff\x32";
+static unsigned char bytes[4];// = "\xe1\x2f\xff\x32";
+
+static int sparc_buffer_read_memory (bfd_vma memaddr, bfd_byte *myaddr, unsigned int length, struct disassemble_info *info)
+{
+	memcpy (myaddr, bytes, length);
+	return 0;
+}
+
+static int symbol_at_address(bfd_vma addr, struct disassemble_info * info)
+{
+	return 0;
+}
+
+
+static void hoho  (int status, bfd_vma memaddr, struct disassemble_info *info)
+{
+
+}
+
+
+static void  print_address(bfd_vma address, struct disassemble_info *info)
+{
+	cons_printf("0x%lx", (address)); // control flags and so
+	return ;
+}
+
+/* Disassembler entry point */
+char *gnu_disparc(unsigned char *inst, unsigned long offset)
+{
+	struct disassemble_info info;
+
+	str[0] = '\0';
+
+	Offset = (unsigned long)offset;
+	//endian_memcpy(bytes, inst, 4); //
+	memcpy(bytes, inst, 4); // TODO handle thumb
+
+	/* prepare disassembler */
+	memset(&info,'\0', sizeof(struct disassemble_info));
+	//info.arch = CPU_LOONGSON_2F; //ARM_EXT_LOONGSON2F|ARM_EXT_V1|ARM_EXT_V4T|ARM_EXT_V5;
+	info.buffer = bytes; //bytes; //&bytes;
+	info.read_memory_func = &sparc_buffer_read_memory;
+	info.symbol_at_address_func = &symbol_at_address;
+	info.memory_error_func = &hoho;
+	info.print_address_func = &print_address;
+	info.endian = 0;//config_get_i("cfg.endian");
+	info.fprintf_func = &cons_fprintf;
+	info.stream = stdout;
+
+	//print_sparc_disassembler_options(stdout);
+	//parse_sparc_dis_options("arch=loongson2f");
+
+	// endian is controlled by radare
+	if (print_insn_sparc (offset, &info) == -1)
+		cons_strcat("  (data)");
+
+	return str;
+}
